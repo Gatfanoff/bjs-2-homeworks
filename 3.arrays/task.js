bed128ced5ae90352737909fa079be988ecc585c
Марат Гатфанов <gatfanovm@mail.ru>
2023-03-23 11:04:40 +0500

    3.Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+   return arr1.every((el, i) => el === arr2[i] && arr1.length === arr2.length);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const people = users.filter(el => el.gender === gender);
+  if (people.length === 0) {
+    return 0;
+ }
+ const averageOld = people.reduce((prev, curr) => prev + curr.age, 0) / people.length;
+ return averageOld;
 }
